0698e1ace9aee16e0e02d1d45c0f8537a2a2cb21
Cynthia <Cynthialiu0805@gmail.com>
2024-03-18 23:10:17 -0400

    add test

--- tests/test_inputCheck.py
+++ tests/test_inputCheck.py
@@ -6,17 +6,17 @@ class TestInputCheck(unittest.TestCase):
         result = inputCheck.load_file('ontario_boundary.geojson')
         self.assertIsNotNone(result)
     
-    # def test_point_in_boundary(self):
-    #     latitude = 44.550356
-    #     longitude = -80.84538
-    #     result = inputCheck.is_within_ontario(latitude, longitude)
-    #     self.assertTrue(result)
+    def test_point_in_boundary(self):
+        latitude = 44.550356
+        longitude = -80.84538
+        result = inputCheck.is_within_ontario(latitude, longitude)
+        self.assertTrue(result)
 
-    # def test_point_not_in_boundary(self):
-    #     latitude = 48.4284
-    #     longitude = -123.3656
-    #     result = inputCheck.is_within_ontario(latitude, longitude)
-    #     self.assertFalse(result)
+    def test_point_not_in_boundary(self):
+        latitude = 48.4284
+        longitude = -123.3656
+        result = inputCheck.is_within_ontario(latitude, longitude)
+        self.assertFalse(result)
     
     def test_value_error(self):
         latitude = "abc"
