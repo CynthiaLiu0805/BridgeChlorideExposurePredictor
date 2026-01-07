262126e4474fc76858aa13c42eef2fb3449fd8f2
unknown <cynthialiu0805@gmail.com>
2026-01-07 15:21:41 -0500

    Read number of column and rows from data.xlsx for different jurisdiction

--- src/database/all_Cl_SAS_cal.py
+++ src/database/all_Cl_SAS_cal.py
@@ -18,16 +18,16 @@ from constant import Constant
 # Because of the annual 2% increase in traffic volume, the AADT and AADTT are
 # calculated as follows:
 def updateAADT(AADT):
-    resultAADT = np.zeros((1645, 95))
-    for i in range(1, 95):
+    resultAADT = np.zeros((Constant.NUM_ROWS, Constant.NUM_YEARS))
+    for i in range(1, Constant.NUM_YEARS):
         resultAADT[:, 0] = AADT
         resultAADT[:, i] = resultAADT[:, i - 1] + resultAADT[:, i - 1] * 0.02
     return resultAADT
 
 
 def updateAADTT(AADTT):
-    resultAADTT = np.zeros((1645, 95))
-    for i in range(1, 95):
+    resultAADTT = np.zeros((Constant.NUM_ROWS, Constant.NUM_YEARS))
+    for i in range(1, Constant.NUM_YEARS):
         resultAADTT[:, 0] = AADTT
         resultAADTT[:, i] = resultAADTT[:, i - 1] + resultAADTT[:, i - 1] * 0.02
     return resultAADTT
--- src/database/constant.py
+++ src/database/constant.py
@@ -4,6 +4,13 @@ are documented in section nine of SRS:
 https://github.com/CynthiaLiu0805/BridgeCorrosion/blob/main/docs/SRS/SRS.pdf
 Some names might differ because some notation could not by typed as code.'''
 
+import pandas as pd
+
+from pathlib import Path
+# Get the project root directory
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+DATA_DIR = PROJECT_ROOT / 'data'
+WEB_PUBLIC_DIR = PROJECT_ROOT / 'src' / 'web' / 'public'
 
 class Constant:
 
@@ -20,3 +27,18 @@ class Constant:
     chloride_ratio = 0.61        # \theta_{chloride}
     d = 3.5                      # d
     ldv_ratio = 6                # \Theta
+
+    df = pd.read_excel(DATA_DIR / "data.xlsx", sheet_name="t1")
+    
+    # Columns except the first one ('Year')
+    columns = df.columns[1:]
+    years = [int(y) for y in columns]
+    
+    # Start/end year and number of years
+    START_YEAR = min(years)
+    END_YEAR = max(years)
+    NUM_YEARS = END_YEAR - START_YEAR + 1
+
+    # Number of rows excluding the header
+    NUM_ROWS = len(df)
+
